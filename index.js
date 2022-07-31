80ce86922629ead8ab0a61ba53355f062751b061
Utkarsh <91658307+utkarsh21123@users.noreply.github.com>
2022-07-31 13:48:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
+var randomImageSource = "dice" + randomDiceImage + ".png"; //dice1.png - dice6.png
+
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
